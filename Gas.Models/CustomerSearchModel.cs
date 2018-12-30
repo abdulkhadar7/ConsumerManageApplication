b730d343bdd81a43fda60de06eee0d0b2a7d93bd
Abdul Khadar <abdulkhadar7@users.noreply.github.com>
2018-12-30 23:28:23 +0530

    Add files via upload

---
+++ Gas.Models/CustomerSearchModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gas.Models
+{
+    public class CustomerSearchModel
+    {
+        public string CustomerName { get; set; }
+        public int ConsumerNumber { get; set; }
+        public DateTime SubmitDate { get; set; }
+        public string Aadhar { get; set; }
+    }
+}
